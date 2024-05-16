d465b2a4a0d59d628a73d08ab7ffb80ec61f94b7
Slava Korolev <vyacheslav.korolev.ru@mail.ru>
2024-05-17 00:05:25 +0700

    1_2

--- attest_of_python.py
+++ attest_of_python.py
@@ -4,3 +4,14 @@ lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
 data.head()
+
+def one_hot_convert(data):
+    keys = set(data)
+    print(keys)
+    df = pd.DataFrame()
+    for k in keys:
+        df[k] = [1 if el == k else 0 for el in data]
+    return df
+one_hot = one_hot_convert(data['whoAmI'])
+print(data.join(one_hot))
+# Спасибо за разбор ДЗ, очень помогло, долго не мог понять
